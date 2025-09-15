7e42f9a250c9e51585790b53ecb3bb8780d5626a
MariG005 <mg207471@gmail.com>
2025-09-15 11:55:07 -0500

    Crear entidades Biblioteca, Pagos, Carrito Compra, Carrito Detalle y Reseñas

---
+++ lib_dominio/Entidades/Bibliotecas.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lib_dominio.Entidades
+{
+    public class Bibliotecas
+    {
+        public int Id { get; set; }
+        public int Usuario { get; set; }
+        public int VideoJuego { get; set; }
+        public DateTime FechaAdquisicion { get; set; }
+
+        [ForeignKey("Usuario")] public Usuarios? _Usuario { get; set; }
+        [ForeignKey("VideoJuego")] public Videojuegos? _Videojuego { get; set; }
+
+    }
+}
