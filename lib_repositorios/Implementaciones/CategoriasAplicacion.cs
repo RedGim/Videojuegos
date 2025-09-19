f69a1c793c07cc4ae5a6e50dc5d1c3c7f03ce914
valeria ochoa <146028502+Pandaazulmarino@users.noreply.github.com>
2025-09-18 20:46:32 -0500

    Update CategoriasAplicacion.cs

--- lib_repositorios/Implementaciones/CategoriasAplicacion.cs
+++ lib_repositorios/Implementaciones/CategoriasAplicacion.cs
@@ -1,4 +1,4 @@
-﻿using lib_dominio.Entidades;
+using lib_dominio.Entidades;
 using lib_repositorios.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,7 +12,25 @@ namespace lib_repositorios.Implementaciones
         {
             this.IConexion = iConexion;
         }
+        public static bool Validar(Categorias Categorias)
+        {
+            if (string.IsNullOrWhiteSpace(Categorias.Nombre))
+            {
+                throw new Exception("El nombre de la categoria es obligatorio.");
+            }
 
+            if (Categorias.Nombre.Length > 50)
+            {
+                throw new Exception("El nombre de la categoria no puede superar los 50 caracteres.");
+            }
+
+            if (Categorias.Nombre.Length > 300)
+            {
+                throw new Exception("El nombre de la descripcion no puede superar los 300 caracteres.");
+            }
+
+            return true;
+        }
         public void Configurar(string StringConexion)
         {
             this.IConexion!.StringConexion = StringConexion;
@@ -24,7 +42,6 @@ namespace lib_repositorios.Implementaciones
                 throw new Exception("lbFaltaInformacion");
             if (entidad!.Id == 0)
                 throw new Exception("lbNoSeGuardo");
-            // Operaciones
             this.IConexion!.Categorias!.Remove(entidad);
             this.IConexion.SaveChanges();
             return entidad;
@@ -37,6 +54,8 @@ namespace lib_repositorios.Implementaciones
             if (entidad.Id != 0)
                 throw new Exception("lbYaSeGuardo");
             // Operaciones
+            if (!Validar(entidad))
+                throw new Exception("lbNoEsValido");
             this.IConexion!.Categorias!.Add(entidad);
             this.IConexion.SaveChanges();
             return entidad;
@@ -54,6 +73,8 @@ namespace lib_repositorios.Implementaciones
             if (entidad!.Id == 0)
                 throw new Exception("lbNoSeGuardo");
             // Operaciones
+            if (!Validar(entidad))
+                throw new Exception("lbNoEsValido");
             var entry = this.IConexion!.Entry<Categorias>(entidad);
             entry.State = EntityState.Modified;
             this.IConexion.SaveChanges();
